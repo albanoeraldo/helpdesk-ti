9b23629d720384d01e7c265871dfb61fd3a71b8e
Eraldo Albano <albanoeraldo.ti@gmail.com>
2025-11-14 16:32:31 -0300

    Implementando o login

---
+++ helpdesk/assets/js/login.js
@@ -0,0 +1,21 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const errorMsg = document.getElementById("errorMsg");
+
+    // Login simples, sem backend
+    const emailCorreto = "admin@helpdesk.com";
+    const senhaCorreta = "123456";
+
+    if (email === emailCorreto && senha === senhaCorreta) {
+        // Salva sessão no navegador
+        localStorage.setItem("logado", "true");
+
+        // Vai para o dashboard
+        window.location.href = "dashboard.html";
+    } else {
+        errorMsg.textContent = "Email ou senha incorretos!";
+    }
+});
--- helpdesk/assets/js/script.js
+++ helpdesk/assets/js/script.js
@@ -4,3 +4,9 @@ const sidebar = document.querySelector('.sidebar');
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('open');
 });
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    localStorage.removeItem("logado");
+    window.location.href = "login.html";
+});
+
